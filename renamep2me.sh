06aadfc7e5b11ecab857667caa3b6feeca517f0b
Jimmy Wan <jimmywan@@users.noreply.github.com>
2024-11-06 15:06:21 -0600

    Change time to noon instead of midnight.

--- renamep2me.sh
+++ renamep2me.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -name '*.jpg' | sed -E 's#([^_]+)_([^_]+)_([^_]+)_([^_]+)_([^_]+)#exiftool -allDates="\2:\3:\4 00:00:00" \1_\2_\3_\4_\5#' | bash
+find . -type f -name '*.jpg' | sed -E 's#([^_]+)_([^_]+)_([^_]+)_([^_]+)_([^_]+)#exiftool -allDates="\2:\3:\4 12:00:00" \1_\2_\3_\4_\5#' | bash
